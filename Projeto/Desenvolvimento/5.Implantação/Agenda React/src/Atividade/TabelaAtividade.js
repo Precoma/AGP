0d6deb50fb5e1dd45b16ed44e261b3ef649d0ef0
Victor Precoma <139169485+Precoma@users.noreply.github.com>
2024-09-22 18:45:02 -0300

    Add files via upload

---
+++ Projeto/Desenvolvimento/5.Implantação/Agenda React/src/Atividade/TabelaAtividade.js
@@ -0,0 +1,37 @@
+function TabelaAtividade({vetor, remover, selecionar}){
+    return(
+
+        <table class="table table-hover table-responsive w-auto table-striped">
+        <thead>
+            <tr>
+                <th scope="col">Atividade:</th>
+                <th scope="col">Data entrega:</th>
+                <th scope="col">Descrição:</th>
+                <th scope="col">Matéria:</th>
+                <th scope="col">Professor:</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+                vetor.map((obj, indice) => (
+                    <tr key={indice}>
+                        <td>{obj.nome}</td>
+                        <td>{obj.data_entrega}</td>
+                        <td>{obj.descricao}</td>
+                        <td>{obj.materia}</td>
+                        <td>{obj.professor}</td>
+                        <td> <button className="btn btn-danger" onClick={() => remover(obj.id)}>Remover</button>  </td>
+                        <td> <button className="btn btn-warning" onClick={() => {selecionar(indice)}}>Editar</button>  </td>
+                    </tr>
+                ))
+
+            }
+        </tbody>
+    </table>
+
+    )
+}
+
+export default TabelaAtividade;
