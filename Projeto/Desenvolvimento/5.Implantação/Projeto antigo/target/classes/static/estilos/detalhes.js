278650283ef87378a73d8a68cb1e8f73da81c2a3
Gustavo Henrique de Camargo <gugacamargo08@outlook.com>
2024-09-22 16:08:00 -0300

    estilos na pagina inicial

---
+++ Projeto/Desenvolvimento/5.Implantação/Projeto antigo/target/classes/static/estilos/detalhes.js
@@ -0,0 +1,8 @@
+function mostrarCatv() {
+	var bloco = document.getElementById('catv')
+	if (bloco.style.display == "flex") {
+    bloco.style.display = "none"; }
+    else {
+    bloco.style.display = "flex"; 
+  }
+}
